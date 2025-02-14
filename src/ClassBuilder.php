da962dcb1e8596e030931dbb22bf3604a459d695
Tycho Huisman <117562628+ithuis@users.noreply.github.com>
2025-02-14 14:24:35 +0100

    Update ClassBuilder.php
    add prefix

--- src/ClassBuilder.php
+++ src/ClassBuilder.php
@@ -9,13 +9,34 @@ class ClassBuilder implements Stringable
 {
     protected $pending = [];
 
+    protected $prefix = "tw-";
+
     public function add($classes)
     {
-        $this->pending[] = Arr::toCssClasses($classes);
+        $classes = Arr::toCssClasses($classes);
+
+        if (!empty($this->prefix)) {
+            $classes = $this->addPrefix($classes, $this->prefix);
+        }
+
+        $this->pending[] = $classes;
 
         return $this;
     }
 
+    public function addPrefix($classes, $prefix = '')
+    {
+        $classes = array_filter(explode(' ', $classes));
+
+        $classes = array_map(function ($class) use ($prefix) {
+            $parts = explode(':', $class);
+            $parts[count($parts) - 1] = $prefix . $parts[count($parts) - 1];
+            return implode(':', $parts);
+        }, $classes);
+
+        return implode(' ', $classes);
+    }
+    
     public function __toString()
     {
         return (string) collect($this->pending)->join(' ');
